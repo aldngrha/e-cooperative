cfdad56d0c2b9ac75fdbd24908e50bf2aade1fd7
aldngrha <aldi.nugrahatk@gmail.com>
2022-10-21 15:04:14 +0700

    add user request

---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            "name" => "required|max:255",
+            "email" => "required|max:255,unique:users",
+            "place_of_birth" => "max:255",
+            "date_of_birth" => "date",
+            "phone_number" => "max:255",
+            "gender" => "max:255",
+            "position" => "max:255",
+            "address" => "",
+        ];
+    }
+}
