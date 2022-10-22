543b8c21dcc623ccc22053e0cc46e3ba109661d3
aldngrha <aldi.nugrahatk@gmail.com>
2022-10-22 13:59:57 +0700

    add resource view option

---
+++ resources/views/pages/admin/option/create.blade.php
@@ -0,0 +1,62 @@
+@extends("layouts.admin.admin")
+
+@section("title")
+    Admin Koperasi - Opsi
+@endsection
+
+@section("content")
+    <div class="container-fluid">
+
+        <!-- Page Heading -->
+        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+
+        </div>
+
+        <div class="card shadow">
+            <div class="card-header">
+                <h3 class="card-title text-primary font-weight-bold">Opsi</h3>
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+            </div>
+
+            <div class="card-body">
+                <form action="{{ route("option.store") }}" method="POST">
+                    @csrf
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label class="interest_rate">Bunga (%)</label>
+                                    <input type="number" name="interest_rate" placeholder="masukkan bunga pinjaman" class="form-control{{ $errors->has('interest_rate') ? ' is-invalid' : '' }}" value="{{ old("interest_rate") }}">
+                                    @if ($errors->has('interest_rate'))
+                                        <span class="invalid-feedback">{{ $errors->first('interest_rate') }}</span>
+                                    @endif
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label class="time_period">Lama Pinjam (dalam bulan)</label>
+                                    <input type="number" name="time_period" placeholder="masukkan lama waktu pinjaman" class="form-control{{ $errors->has('time_period') ? ' is-invalid' : '' }}" value="{{ old("time_period") }}">
+                                    @if ($errors->has('time_period'))
+                                        <span class="invalid-feedback">{{ $errors->first('time_period') }}</span>
+                                    @endif
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-footer text-right">
+                        <button type="submit" class="btn btn-primary">Simpan</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <!-- /.container-fluid -->
+    </div>
+@endsection
---
+++ resources/views/pages/admin/option/index.blade.php
@@ -0,0 +1,64 @@
+@extends("layouts.admin.admin")
+
+@section("title")
+    Dashboard Admin Koperasi
+@endsection
+
+@section("content")
+    <!-- Begin Page Content -->
+    <div class="container-fluid">
+
+        <!-- Page Heading -->
+
+        <!-- DataTales Example -->
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h1 class="h3 mb-2 text-primary font-weight-bold">Opsi</h1>
+                @if($items->isEmpty())
+                    <a href="{{ route('option.create') }}" class="btn btn-sm btn-pill btn-primary">Tambah Simpanan</a>
+                @endif
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-striped table-vcenter text-nowrap" id="dataTable" width="100%" cellspacing="0">
+                        <thead>
+                            <tr>
+                                <th>Bunga (%)</th>
+                                <th>Lama Pinjam</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        @forelse ($items as $item)
+                            <tr>
+                                <td>{{ $item->interest_rate }}</td>
+                                <td>{{ $item->time_period }}</td>
+                                <td>
+                                    <a href="{{ route('option.edit', $item->id) }}" class="btn btn-info">
+                                        <i class="fas fa-pencil-alt"></i>
+                                    </a>
+                                    <form action="{{ route('option.destroy', $item->id) }}" class="d-inline" method="POST">
+                                        @csrf
+                                        @method('delete')
+                                        <button class="btn btn-danger">
+                                            <i class="fas fa-trash"></i>
+                                        </button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @empty
+                            <tr>
+                                <td colspan="7" class="text-center">
+                                    Data kosong
+                                </td>
+                            </tr>
+                        @endforelse
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <!-- /.container-fluid -->
+@endsection
