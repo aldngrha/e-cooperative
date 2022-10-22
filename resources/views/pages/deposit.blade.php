92deb3df65c703ce92f3495a7c879acf0c9136d1
aldngrha <aldi.nugrahatk@gmail.com>
2022-10-22 17:33:39 +0700

    add deposit view

---
+++ resources/views/pages/deposit.blade.php
@@ -0,0 +1,74 @@
+@extends("layouts.app")
+
+@section("title")
+    Dashboard Admin Koperasi
+@endsection
+
+@section("content")
+    <div class="container-fluid">
+
+        <!-- Page Heading -->
+        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+
+        </div>
+
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @elseif(session()->has("message"))
+            <div class="alert alert-success">
+                <p>{{  session()->get("message") }}</p>
+            </div>
+        @endif
+
+        <div class="card shadow">
+            <div class="card-header">
+                <h3 class="card-title text-primary font-weight-bold">Pengajuan Simpanan Pokok</h3>
+                <div class="card-options">
+                    <a href="{{ route('home') }}" class="btn btn-sm btn-outline-primary">Kembali</a>
+                </div>
+            </div>
+            <div class="card-body">
+                <form action="{{ route("checkout") }}" method="POST">
+                    @csrf
+                    <div class="card-body">
+                        <div class="form-group">
+                            <div class="row align-items-center">
+                                <label class="col-sm-2" for="amount_deposit">Jumlah Simpanan</label>
+                                <div class="col-sm-10">
+                                    <div class="input-group">
+                                    <span class="input-group-prepend">
+                                        <span class="input-group-text">Rp</span>
+                                    </span>
+                                    <input type="number" name="amount_deposit" autocomplete="off"
+                                       class="form-control{{ $errors->has('amount_deposit') ? ' is-invalid' : '' }}"
+                                       value="{{ old('amount_deposit') }}">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="row align-items-center">
+                                <label class="col-sm-2" for="amount_deposit">Keterangan</label>
+                                <div class="col-sm-10">
+                                    <input type="text" name="description" autocomplete="off"
+                                           class="form-control{{ $errors->has('description') ? ' is-invalid' : '' }}"
+                                           value="{{ old('description') }}">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-footer text-right">
+                        <button type="submit" class="btn btn-primary">Ajukan Simpanan</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <!-- /.container-fluid -->
+    </div>
+@endsection
