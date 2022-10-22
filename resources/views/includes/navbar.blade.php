dc98ed05364b92fa35b1986d4dfe02d58acef23f
aldngrha <aldi.nugrahatk@gmail.com>
2022-10-22 18:51:32 +0700

    update navbar

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -22,7 +22,7 @@
       <!-- Dropdown - User Information -->
       <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
            aria-labelledby="userDropdown">
-        <a class="dropdown-item" href="#">
+        <a class="dropdown-item" href="{{ route("edit") }}">
           <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
           Profil Anggota
         </a>
