36030f6a73684ff9ee7e95851dfd57d3794af0fc
aldngrha <aldi.nugrahatk@gmail.com>
2022-10-22 13:58:42 +0700

    created option controller

---
+++ app/Http/Controllers/Admin/OptionController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\OptionRequest;
+use App\Models\Option;
+use Illuminate\Http\Request;
+
+class OptionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $items = Option::all();
+        return view("pages.admin.option.index", [
+            "items" => $items
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view("pages.admin.option.create");
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(OptionRequest $request)
+    {
+        $data = $request->all();
+
+        Option::create($data);
+        return redirect()->route("option.index");
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $item = Option::findOrFail($id);
+
+        return view("pages.admin.option.edit", [
+            "item" => $item,
+        ]);
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(OptionRequest $request, $id)
+    {
+        $data = $request->all();
+
+        $item = Option::findOrFail($id);
+        $item->update($data);
+
+        return redirect()->route("option.index");
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $item = Option::findOrFail($id);
+        $item->delete();
+
+        return redirect()->route("option.index");
+    }
+}
