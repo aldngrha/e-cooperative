310d6050d837e9db35489b32b7e04ea62affb317
aldngrha <aldi.nugrahatk@gmail.com>
2022-10-24 06:25:37 +0700

    delete option id

--- app/Models/DepositMust.php
+++ app/Models/DepositMust.php
@@ -11,16 +11,12 @@ class DepositMust extends Model
     use HasFactory;
     use SoftDeletes;
 
-    protected $fillable = ["users_id", "option_id", "amount_deposit", "description"];
+    protected $fillable = ["users_id", "amount_deposit", "description"];
 
     protected $hidden = [];
 
     public function members() {
         return $this->belongsTo(User::class, "users_id", "id");
-    }
-
-    public function options() {
-        return $this->belongsTo(Option::class, "option_id", "id");
     }
 
 }
