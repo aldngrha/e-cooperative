fecf17a8332ac7f10b87a30a9254115eadc34518
aldngrha <aldi.nugrahatk@gmail.com>
2022-10-24 06:27:57 +0700

    add route deposit must

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\DepositController;
+use App\Http\Controllers\DepositMustController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\OptionController;
@@ -23,6 +24,8 @@ Route::middleware(["auth","user"])->group(function () {
     Route::get("/", [HomeController::class, "index"])->name("home");
     Route::get("/deposit", [DepositController::class, "index"])->name("deposit");
     Route::post("/deposit", [DepositController::class, "process"])->name("checkout");
+    Route::get("/deposit-must", [DepositMustController::class, "index"])->name("deposit-must");
+    Route::post("/deposit-must", [DepositMustController::class, "process"])->name("checkout-must");
     Route::get("/profile/edit", [UserController::class, "edit"])->name("edit");
     Route::put("/profile/edit", [UserController::class, "update"])->name("update");
 });
