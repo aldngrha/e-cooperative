ed2ffca80129b555f345a6233f98d3fbdc3d87f4
aldngrha <aldi.nugrahatk@gmail.com>
2022-10-24 06:26:17 +0700

    add deposit must request

---
+++ app/Http/Requests/DepositMustRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class DepositMustRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            "users_id" => "integer",
+            "amount_deposit" => "required|integer",
+            "description" => "required"
+        ];
+    }
+}
