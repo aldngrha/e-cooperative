cbe0010552ffc70cd7fdbd35b3199640ad90b53d
aldngrha <aldi.nugrahatk@gmail.com>
2022-10-24 16:30:10 +0700

    update loan model

--- app/Models/Loan.php
+++ app/Models/Loan.php
@@ -4,17 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Loan extends Model
 {
     use HasFactory;
     use SoftDeletes;
 
-    protected $fillable = ["members_id", "amount_loan", "due_date", "description"];
+    protected $fillable = ["users_id", "option_id", "amount_loan", "due_date", "description", "status"];
 
     protected $hidden = [];
 
     public function members() {
-        $this->belongsTo(Member::class, "members_id", "id");
+        $this->belongsTo(User::class, "users_id", "id");
+    }
+
+    public function options() {
+        return $this->belongsTo(Option::class, "option_id", "id");
     }
 }
