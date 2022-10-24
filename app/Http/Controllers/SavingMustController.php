1cb021439c0080bc69f53dc0c83048ef2865e083
aldngrha <aldi.nugrahatk@gmail.com>
2022-10-24 16:28:05 +0700

    update saving must controller

--- app/Http/Controllers/SavingMustController.php
+++ app/Http/Controllers/SavingMustController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class SavingController extends Controller
+class SavingMustController extends Controller
 {
     public function index(Request $request) {
         return view("pages.saving-must");
