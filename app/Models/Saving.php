7c247426431e141da28db612d2fc86679e7106b9
aldngrha <aldi.nugrahatk@gmail.com>
2022-10-24 16:29:26 +0700

    update saving model

--- app/Models/Saving.php
+++ app/Models/Saving.php
@@ -10,12 +10,15 @@ class Saving extends Model
     use HasFactory;
     use SoftDeletes;
 
-    protected $fillable = ["members_id", "amount_save", "description"];
+    protected $fillable = ["users_id", "deposit_id", "amount_save", "description"];
 
     protected $hidden = [];
 
     public function members() {
-        return $this->belongsTo(Member::class, "members_id", "id");
+        return $this->belongsTo(User::class, "users_id", "id");
     }
 
+    public function deposits() {
+        return $this->belongsTo(Option::class, "option_id", "id");
+    }
 }
