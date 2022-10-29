68166af2f2f40bfdb5f183350a09345981a7242d
aldngrha <aldi.nugrahatk@gmail.com>
2022-10-29 20:53:54 +0700

    add edit to loan view admin

---
+++ resources/views/pages/admin/loan/edit.blade.php
@@ -0,0 +1,57 @@
+@extends("layouts.admin.admin")
+
+@section("title")
+    Dashboard Admin Koperasi
+@endsection
+
+@section("content")
+    <div class="container-fluid">
+
+        <!-- Page Heading -->
+        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+
+        </div>
+
+        <div class="card shadow">
+            <div class="card-header">
+                <h3 class="card-title text-primary font-weight-bold">Ubah Status Pinjaman {{ $loan->members->name }}</h3>
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+            </div>
+
+            <div class="card-body">
+                <form action="{{ route("loan.update", $loan->id) }}" method="POST">
+                    @method("put")
+                    @csrf
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label class="status">Status Pinjaman</label>
+                                    <select name="status" required class="form-control">
+                                        <option value="{{ $loan->status }}">Tentukan Status Pinjaman</option>
+                                        <option value="TERTUNDA">TERTUNDA</option>
+                                        <option value="BELUM LUNAS">BELUM LUNAS</option>
+                                        <option value="LUNAS">LUNAS</option>
+                                        <option value="GAGAL">GAGAL</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-footer text-right">
+                        <button type="submit" class="btn btn-primary">Simpan</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <!-- /.container-fluid -->
+    </div>
+@endsection
