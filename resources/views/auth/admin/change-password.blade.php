e29fef663684b305b27d8b21afb07b659cdc1c45
aldngrha <aldi.nugrahatk@gmail.com>
2022-11-06 15:42:05 +0700

    create view change password admin

---
+++ resources/views/auth/admin/change-password.blade.php
@@ -0,0 +1,84 @@
+@extends("layouts.admin.admin")
+
+@section("title")
+    Ubah Kata Sandi
+@endsection
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 offset-1">
+                <div class="card card-body mt-5 mb-5">
+                    <h2 class="ml-3 mb-3 text-primary font-weight-bold">Ubah Kata Sandi</h2>
+
+                    <div class="card-body">
+                        @if (session('error'))
+                            <div class="alert alert-danger">
+                                {{ session('error') }}
+                            </div>
+                        @endif
+                        @if (session('success'))
+                            <div class="alert alert-success">
+                                {{ session('success') }}
+                            </div>
+                        @endif
+                        @if($errors)
+                            @foreach ($errors->all() as $error)
+                                <div class="alert alert-danger">{{ $error }}</div>
+                            @endforeach
+                        @endif
+                        <form class="form-horizontal" method="POST" action="{{ route('change-password') }}">
+                            {{ csrf_field() }}
+
+                            <div class="form-group{{ $errors->has('current-password') ? ' has-error' : '' }}">
+                                <label for="new-password" class="col-md-4 control-label">Kata Sandi Sekarang</label>
+
+                                <div class="col-md-6">
+                                    <input id="current-password" type="password" class="form-control" name="current-password" required>
+
+                                    @if ($errors->has('current-password'))
+                                        <span class="help-block">
+                  <strong>{{ $errors->first('current-password') }}</strong>
+                </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group{{ $errors->has('new-password') ? ' has-error' : '' }}">
+                                <label for="new-password" class="col-md-4 control-label">Kata Sandi Baru</label>
+
+                                <div class="col-md-6">
+                                    <input id="new-password" type="password" class="form-control" name="new-password" required>
+
+                                    @if ($errors->has('new-password'))
+                                        <span class="help-block">
+                  <strong>{{ $errors->first('new-password') }}</strong>
+                </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="new-password-confirm" class="col-md-4 control-label">Konfirmasi Kata Sandi</label>
+
+                                <div class="col-md-6">
+                                    <input id="new-password-confirm" type="password" class="form-control" name="new-password_confirmation"
+                                           required>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <div class="col-md-6 col-md-offset-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        Ubah Kata Sandi
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
