d1fa17dd8b5595b481e03da685d9c3cf329c3d90
aldngrha <aldi.nugrahatk@gmail.com>
2022-11-06 14:07:13 +0700

    add last 10 loan and pie cart view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,6 +25,7 @@ class HomeController extends Controller
         $deposit = Deposit::all();
         $depositMust = DepositMust::all();
         $loan = Loan::with(["options"])->firstOrFail();
+        $loans = Loan::with(["members"])->orderBy("id", "DESC")->take(10)->get();
 
         $amount_deposit = $deposit->sum("amount_deposit");
         $amount_deposit_must = $depositMust->sum("amount_deposit");
@@ -33,17 +34,22 @@ class HomeController extends Controller
         $amount_loan = $loan->sum("amount_loan");
 
         $rate = $loan->options->interest_rate;
-
         $total_loan = $amount_loan - $loan_status;
-
         $with_rate = ($loan_status * $rate) / 100 + $loan_status;
-
         $total_saldo = $amount_deposit + $amount_deposit_must - $amount_loan + $with_rate;
+
+        $pie = [
+            "tertunda" => Loan::where("status", "TERTUNDA")->count(),
+            "lunas" => Loan::where("status", "LUNAS")->count(),
+            "belum_lunas" => Loan::where("status", "BELUM LUNAS")->count(),
+        ];
         return view('pages.home', [
+            "loans" => $loans,
             "total" => $total_saldo,
             "deposit" => $amount_deposit,
             "deposit_must" => $amount_deposit_must,
             "total_loan" => $total_loan,
+            "pie" => $pie
         ]);
     }
 }
