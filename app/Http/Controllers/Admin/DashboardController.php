d46a5debe2405fd9d4bc2f394a65988eb876973c
aldngrha <aldi.nugrahatk@gmail.com>
2022-11-07 17:15:29 +0700

    fixing bug option

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Deposit;
 use App\Models\DepositMust;
 use App\Models\Loan;
+use App\Models\Option;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -15,18 +16,21 @@ class DashboardController extends Controller
 
         $deposit = Deposit::all();
         $depositMust = DepositMust::all();
-        $loan = Loan::with(["options"])->firstOrFail();
+        $loan = Loan::all();
+        $option = Option::firstOrFail();
         $loans = Loan::with(["members"])->orderBy("id", "DESC")->take(10)->get();
 
         $amount_deposit = $deposit->sum("amount_deposit");
         $amount_deposit_must = $depositMust->sum("amount_deposit");
 
         $loan_status = $loan->where("status", "LUNAS")->sum("amount_loan");
+        $loan_status_pending = $loan->where("status", "TERTUNDA")->sum("amount_loan");
         $amount_loan = $loan->sum("amount_loan");
-        $rate = $loan->options->interest_rate;
-        $total_loan = $amount_loan - $loan_status;
+
+        $rate = $option->interest_rate;
+        $total_loan = $amount_loan - $loan_status - $loan_status_pending;
         $with_rate = ($loan_status * $rate) / 100 + $loan_status;
-        $total_saldo = $amount_deposit + $amount_deposit_must - $amount_loan + $with_rate;
+        $total_saldo = $amount_deposit + $amount_deposit_must - $amount_loan + $with_rate + $loan_status_pending;
 
         $pie = [
             "tertunda" => Loan::where("status", "TERTUNDA")->count(),
