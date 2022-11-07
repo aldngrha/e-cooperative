14b584d124a463a09d1fc9bf68c0ce0f75d83caf
aldngrha <aldi.nugrahatk@gmail.com>
2022-11-07 17:17:38 +0700

    fixing bug option

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Deposit;
 use App\Models\DepositMust;
 use App\Models\Loan;
+use App\Models\Option;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -24,25 +25,28 @@ class HomeController extends Controller
     {
         $deposit = Deposit::all();
         $depositMust = DepositMust::all();
-        $loan = Loan::with(["options"])->firstOrFail();
+        $loan = Loan::all();
+        $option = Option::firstOrFail();
         $loans = Loan::with(["members"])->orderBy("id", "DESC")->take(10)->get();
 
         $amount_deposit = $deposit->sum("amount_deposit");
         $amount_deposit_must = $depositMust->sum("amount_deposit");
 
-        $loan_status = $loan->where("status", "LUNAS")->sum("amount_loan");
+        $loan_status_paid = $loan->where("status", "LUNAS")->sum("amount_loan");
+        $loan_status_pending = $loan->where("status", "TERTUNDA")->sum("amount_loan");
         $amount_loan = $loan->sum("amount_loan");
 
-        $rate = $loan->options->interest_rate;
-        $total_loan = $amount_loan - $loan_status;
-        $with_rate = ($loan_status * $rate) / 100 + $loan_status;
-        $total_saldo = $amount_deposit + $amount_deposit_must - $amount_loan + $with_rate;
+        $rate = $option->interest_rate;
+        $total_loan = $amount_loan - $loan_status_paid - $loan_status_pending;
+        $with_rate = ($loan_status_paid * $rate) / 100 + $loan_status_paid;
+        $total_saldo = $amount_deposit + $amount_deposit_must - $amount_loan + $with_rate + $loan_status_pending;
 
         $pie = [
             "tertunda" => Loan::where("status", "TERTUNDA")->count(),
             "lunas" => Loan::where("status", "LUNAS")->count(),
             "belum_lunas" => Loan::where("status", "BELUM LUNAS")->count(),
         ];
+
         return view('pages.home', [
             "loans" => $loans,
             "total" => $total_saldo,
