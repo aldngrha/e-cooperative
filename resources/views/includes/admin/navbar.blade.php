bdff24f0988a5af5630d3175c32b0bb0a28b5ea2
aldngrha <aldi.nugrahatk@gmail.com>
2022-11-07 17:28:12 +0700

    creating avatar intial name for admin roles

--- resources/views/includes/admin/navbar.blade.php
+++ resources/views/includes/admin/navbar.blade.php
@@ -16,8 +16,8 @@
       <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
          data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
         <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ Auth::user()->name }}</span>
-        <img class="img-profile rounded-circle"
-             src="{{ url("backend/img/undraw_profile.svg") }}">
+        <img class="img-profile rounded-circle" alt="Avatar {{ Auth::user()->name }}"
+             src="https://ui-avatars.com/api/?name={{ Auth::user()->name }}&background=random">
       </a>
       <!-- Dropdown - User Information -->
       <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
