c14e5cec3f2ef1dfdb4212296288717342d9f5b8
aldngrha <aldi.nugrahatk@gmail.com>
2023-02-15 11:22:36 +0700

    create view saving voluntary controller

---
+++ app/Http/Controllers/Admin/SavingVoluntaryController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\DepositVoluntary;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class SavingVoluntaryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $items = DepositVoluntary::with(["members"])->orderBy("id", "DESC")->get();
+        return view("pages.admin.saving-voluntary.index", [
+            "items" => $items,
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+//        return view("pages.admin.saving.create");
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $user = User::with(["deposits"])->findOrFail($id);
+        $showSum = $user->deposits()->sum("amount_deposit");
+        return view("pages.admin.saving.saving-detail", [
+            "user" => $user,
+            "showSum" => $showSum
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
