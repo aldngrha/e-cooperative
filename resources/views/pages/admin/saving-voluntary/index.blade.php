17988410a856c9de94db184ea3697b4faa18c765
aldngrha <aldi.nugrahatk@gmail.com>
2023-02-15 11:22:02 +0700

    create view saving-voluntary

---
+++ resources/views/pages/admin/saving-voluntary/index.blade.php
@@ -0,0 +1,60 @@
+@extends("layouts.admin.admin")
+
+@section("title")
+    Simpanan Pokok - Admin Koperasi
+@endsection
+
+@section("content")
+    <!-- Begin Page Content -->
+    <div class="container-fluid">
+
+        <!-- Page Heading -->
+
+        <!-- DataTales Example -->
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h1 class="h3 mb-2 text-primary font-weight-bold">Tabel Simpanan Sukarela</h1>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-striped table-vcenter text-nowrap" id="dataTable" width="100%" cellspacing="0">
+                        <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Nama Anggota</th>
+                            <th>Jumlah Simpan</th>
+                            <th>Keterangan</th>
+                            <th>Waktu Pengajuan</th>
+                            <th>Action</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @forelse ($items as $item)
+                            <tr>
+                                <td>{{ $loop->iteration }}</td>
+                                <td>{{ $item->members->name }}</td>
+                                <td>Rp {{ number_format($item->amount_deposit,0,".",".") }}</td>
+                                <td>{{ $item->description }}</td>
+                                <td>{{ \Carbon\Carbon::parse($item->created_at)->isoFormat("dddd, D MMMM YYYY") }}</td>
+                                <td>
+                                    <a href="{{ route('saving.show', $item->members->id) }}" class="btn btn-info">
+                                        <i class="fas fa-eye"></i>
+                                    </a>
+                                </td>
+                            </tr>
+                        @empty
+                            <tr>
+                                <td colspan="7" class="text-center">
+                                    Data kosong
+                                </td>
+                            </tr>
+                        @endforelse
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <!-- /.container-fluid -->
+@endsection
