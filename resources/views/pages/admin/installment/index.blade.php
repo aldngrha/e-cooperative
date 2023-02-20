1389edd1d06b6479cfba1c644a3b5d732b7ec7ff
aldngrha <aldi.nugrahatk@gmail.com>
2023-02-20 18:16:09 +0700

    create page installment

---
+++ resources/views/pages/admin/installment/index.blade.php
@@ -0,0 +1,65 @@
+@extends("layouts.admin.admin")
+
+@section("title")
+    Simpanan Pokok - Admin Koperasi
+@endsection
+
+@section("content")
+    <!-- Begin Page Content -->
+    <div class="container-fluid">
+
+        <!-- Page Heading -->
+
+        <!-- DataTales Example -->
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h1 class="h3 mb-2 text-primary font-weight-bold">Tabel Angsuran</h1>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-striped table-vcenter text-nowrap" id="dataTable" width="100%" cellspacing="0">
+                        <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Kode Pinjam</th>
+                            <th>Nama Anggota</th>
+                            <th>Angsuran Ke</th>
+                            <th>Jumlah Angsuran</th>
+                            <th>Waktu Pembayaran</th>
+                            <th>Action</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @forelse ($items as $item)
+                            <tr>
+                                <td>{{ $loop->iteration }}</td>
+                                <td>{{ $item->loans->loan_code }}</td>
+                                <td>{{ $item->loans->members->name }}</td>
+                                <td>{{ $item->installment_number }}</td>
+                                <td>Rp {{ number_format($item->amount_installment,0,".",".") }}</td>
+                                <td>{{ \Carbon\Carbon::parse($item->created_at)->isoFormat("dddd, D MMMM YYYY") }}</td>
+                                <td>
+                                    <a href="{{ route('installment.show', $item->loans->members->id) }}" class="btn btn-info">
+                                        <i class="fas fa-eye"></i>
+                                    </a>
+                                    <a href="{{ route('installment.edit', $item->id) }}" class="btn btn-warning">
+                                        <i class="fas fa-pencil-alt"></i>
+                                    </a>
+                                </td>
+                            </tr>
+                        @empty
+                            <tr>
+                                <td colspan="7" class="text-center">
+                                    Data kosong
+                                </td>
+                            </tr>
+                        @endforelse
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <!-- /.container-fluid -->
+@endsection
