1398e7f7ada14e9c591c96fe12e763314e23cf3d
aldngrha <aldi.nugrahatk@gmail.com>
2023-03-01 15:05:10 +0700

    add seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\DepositMust;
+use App\Models\DepositVoluntary;
+use App\Models\Installment;
+use App\Models\Loan;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -17,5 +22,12 @@ class DatabaseSeeder extends Seeder
         $this->call([OptionsTableSeeder::class]);
         $this->call([LoansTableSeeder::class]);
         $this->call([InstallmentsTableSeeder::class]);
+        $this->call([DepositMustsTableSeeder::class]);
+        $this->call([DepositVolutariesTableSeeder::class]);
+//
+//        User::factory(10)->create();
+//        DepositMust::factory(10)->create();
+//        DepositVoluntary::factory(10)->create();
+//        Loan::factory(10)->create();
     }
 }
